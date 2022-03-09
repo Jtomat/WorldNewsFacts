1d207a63626908ee2a6160bee795fc0c82bcf901
Timofey Myachikov <57thrasher57@gmail.com>
2022-03-10 03:36:50 +0500

    Adding the back

---
+++ server/routes/userRouter.js
@@ -0,0 +1,14 @@
+const Router = require('express');
+const router = new Router();
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.get('/auth', authMiddleware, userController.check);
+router.put('/updateUser', authMiddleware, userController.updateCredentials);
+router.get('/userById', userController.findUserById);
+router.get('/getAll', userController.getAll);
+router.delete('/deleteUser', userController.removeUser);
+
+module.exports = router;
